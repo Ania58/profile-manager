b6f88285173b74b58b1f4918898b96c39a8e2bec
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-04-25 23:29:29 +0200

    feat: set up flexible Profile typings with mapped and optional types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-type Profile = {
-    name: string;
-    age: number;
-    hobbies: string[];
-}
+const profile = {
+    name: 'Caroline',
+    age: 30,
+    hobbies: ['photography', 'dance', 'guitar', 'writing']
+};
+
+type Profile = typeof profile;
+
+type ProfileKeys = keyof Profile;
+
+type ProfileHobbies = Profile['hobbies'];
+
+type ProfileVisibility = {
+    [key in ProfileKeys]? : boolean
+};
